f99520fe1083a6be71a2debac21163d592a029d8
greenvc <greenvc@126.com>
2017-12-08 17:23:08 +0800

    简单工厂模式

---
+++ src/AbstractFactory/Client.java
@@ -0,0 +1,8 @@
+package AbstractFactory;
+
+public class Client {
+    public static void main(String[]args){
+        ComputerEngineer cf = new ComputerEngineer();
+        cf.makeComputer(1,1);
+    }
+}
