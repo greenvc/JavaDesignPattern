793aaa54617f2297b78632352d27b22d29df9974
greenvc <greenvc@126.com>
2017-12-08 17:27:39 +0800

    抽象工厂方法

--- src/AbstractFactory/Client.java
+++ src/AbstractFactory/Client.java
@@ -2,7 +2,14 @@ package AbstractFactory;
 
 public class Client {
     public static void main(String[]args){
-        ComputerEngineer cf = new ComputerEngineer();
-        cf.makeComputer(1,1);
+//        ComputerEngineer cf = new ComputerEngineer();
+//        cf.makeComputer(1,1);
+    	
+        //创建装机工程师对象
+    	ComputerEngineerAbstract cf = new ComputerEngineerAbstract();
+        //客户选择并创建需要使用的产品对象
+        AbstractFactory af = new IntelFactory();
+        //告诉装机工程师自己选择的产品，让装机工程师组装电脑
+        cf.makeComputer(af);
     }
 }
---
+++ src/AbstractFactory/IntelFactory.java
@@ -0,0 +1,15 @@
+package AbstractFactory;
+
+public class IntelFactory implements AbstractFactory {
+
+	@Override
+	public Cpu createCpu() {
+        return new IntelCpu(755);
+	}
+
+	@Override
+	public Mainboard createMainboard() {
+        return new IntelMainboard(755);
+	}
+
+}
