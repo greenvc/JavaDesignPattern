0a3742720b020355658be23a23a27fd5e6d80fa3
greenvc <greenvc@126.com>
2017-12-08 18:34:48 +0800

    懒汉式单例类

---
+++ src/Singletons/LazySingleton.java
@@ -0,0 +1,21 @@
+package Singletons;
+
+/*
+ * 懒汉式单例类
+ */
+public class LazySingleton {
+    private static LazySingleton instance = null;
+    /**
+     * 私有默认构造子
+     */
+    private LazySingleton(){}
+    /**
+     * 静态工厂方法
+     */
+    public static synchronized LazySingleton getInstance(){
+        if(instance == null){
+            instance = new LazySingleton();
+        }
+        return instance;
+    }
+}
